3980ae93aab9763bbbff0df62fb25bb5883599e2
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-04 22:17:39 +0000

    update

--- scanner/utils/write_markdown.py
+++ scanner/utils/write_markdown.py
@@ -1,4 +1,6 @@
-from mdutils.mdutils import MdUtils
+from pathlib import Path
+
+from pandas import DataFrame
 from structlog import get_logger, stdlib
 
 from .types import TechReport
@@ -12,14 +14,48 @@ def write_output_file(tech_report: TechReport) -> None:
     Args:
         tech_report (TechReport): The tech report to write to a file.
     """
-    md_file = MdUtils(file_name="tech_report", title="Tech Report")
-    md_file.new_header(level=1, title="Tech Report")
-    md_file.new_line("This report summarises the technologies and frameworks used in the repositories.")
-    md_file.new_header(level=2, title="Summary")
-    md_file.new_line("The following is a summary of the technologies and frameworks used in the repositories.")
-    rows = ["Technology/Framework", "Count"]
-    for summary in tech_report["summary"]:
-        rows.append(summary["technology"])
-        rows.append(str(summary["count"]))
-    md_file.new_table(columns=2, rows=len(tech_report["summary"]) + 1, text=rows, text_align="center")
-    md_file.create_md_file()
+    markdown_file = MarkdownFile(file_path="tech_report.md")
+    markdown_file.add_header(level=1, title="Tech Report")
+    markdown_file.add_header(level=2, title="Summary")
+    logger.warning(DataFrame(tech_report["summary"]).to_markdown(index=False))
+    markdown_file.add_table(DataFrame(tech_report["summary"]))
+    markdown_file.add_header(level=2, title="Repositories")
+    for repository in tech_report["repositories"]:
+        markdown_file.add_header(level=3, title=repository["project_name"])
+        markdown_file.add_table(DataFrame(repository["technologies_and_frameworks"]))
+
+
+class MarkdownFile:
+    """Generate a markdown file."""
+
+    lines_of_content: list[str]
+
+    def __init__(self, file_path: str) -> None:
+        """Initialise the markdown file."""
+        self.file_path = file_path
+        self.lines_of_content = []
+
+    def write(self, content: str) -> None:
+        """Write the content to the markdown file."""
+        with Path(self.file_path).open("w", encoding="utf-8") as file:
+            file.writelines(content)
+
+    def _check_last_line_is_empty(self) -> bool:
+        """Check if the last line is empty."""
+        return self.lines_of_content[-1] == ""
+
+    def add_header(self, level: int, title: str) -> None:
+        """Add a header to the markdown file."""
+        if not self._check_last_line_is_empty():
+            self.lines_of_content.append("")
+        self.lines_of_content += [f"{'#' * level} {title}", ""]
+
+    def add_paragraph(self, paragraph: str) -> None:
+        """Add a paragraph to the markdown file."""
+        self.lines_of_content.append(f"{paragraph}")
+
+    def add_table(self, dataframe: DataFrame) -> None:
+        """Add a table to the markdown file."""
+        logger.warning(dataframe.to_markdown(index=False))
+        self.lines_of_content.append(dataframe.to_markdown(index=False))
+        self.lines_of_content.append("")
