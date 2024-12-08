9cf27a6033ebb2f655d52546c38d3c699893a0fb
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-08 16:54:11 +0000

    update

--- scanner/utils/write_markdown.py
+++ scanner/utils/write_markdown.py
@@ -54,13 +54,13 @@ class MarkdownFile:
 
     def _check_last_line_is_empty(self: Self) -> bool:
         """Check if the last line is empty."""
-        return self.lines_of_content[-1] == ""
+        return "\n" in self.lines_of_content[-1]
 
     def add_header(self: Self, level: int, title: str) -> None:
         """Add a header to the markdown file."""
         if self.lines_of_content and not self._check_last_line_is_empty():
-            self.lines_of_content.append("")
-        self.lines_of_content += [f"{'#' * level} {title}", ""]
+            self.lines_of_content[-1] += "\n"
+        self.lines_of_content.append(f"{'#' * level} {title}\n")
 
     def add_paragraph(self: Self, paragraph: str) -> None:
         """Add a paragraph to the markdown file."""
@@ -69,12 +69,9 @@ class MarkdownFile:
     def add_table(self: Self, table_contents: list[dict]) -> None:
         """Add a table to the markdown file."""
         headers = table_contents[0].keys()
-        self.lines_of_content.append("| " + " | ".join(headers) + " |")
-        self.lines_of_content.append("|" + "|".join(["-" * len(header) for header in headers]) + "|")
+        self.lines_of_content.append("| " + " | ".join(headers) + " |\n")
+        self.lines_of_content.append("|" + "|".join(["-" * len(header) for header in headers]) + "|\n")
         for row in table_contents:
             row_values = (str(value) for value in row.values())
-            self.lines_of_content.append("|" + "|".join(row_values) + "|")
+            self.lines_of_content.append("|" + "|".join(row_values) + "|\n")
         logger.warning(self.lines_of_content)
-        # logger.warning(dataframe.to_markdown(index=False))
-        # self.lines_of_content.append(dataframe.to_markdown(index=False))
-        self.lines_of_content.append("")
