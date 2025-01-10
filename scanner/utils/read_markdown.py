2a0b8ce8ace2f076b3d26d143a839825603ace32
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-10 14:09:16 +0000

    update

--- scanner/utils/read_markdown.py
+++ scanner/utils/read_markdown.py
@@ -90,17 +90,14 @@ def find_markdown_badges(line_contents: str) -> list[TechnologiesAndFrameworksDe
     Returns:
         list[str]: The list of technologies and frameworks used in the repository.
     """
-    badge_matches = findall(r"(![\[][^\)]*\]\([^\)]*\))", line_contents)
+    badge_matches = findall(r"!\[([^\]]*)\]\(([^)]*)\)", line_contents)
     if not badge_matches:
         return []
     logger.debug("Found badges", badges=badge_matches)
-    badge_names = [
-        badge_match.split("](")[0].replace("![", "") for badge_match in badge_matches
-    ]
     return [
         {
             "technology": badge_match[0],
-            "badge": badge_match[1],
+            "badge": f"![{badge_match[0]}]({badge_match[1]})",
         }
-        for badge_match in zip(badge_names, badge_matches, strict=True)
+        for badge_match in badge_matches
     ]
