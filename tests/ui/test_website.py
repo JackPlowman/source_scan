d60c45d5d71089bc672c84d01391d0022347e877
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-18 22:41:37 +0100

    test: Add More UI Tests (#305)
    # Pull Request
    
    ## Description
    
    This pull request adds several new test cases to the
    `tests/ui/test_website.py` file to improve test coverage for the web
    application's UI. The tests focus on verifying the presence and
    structure of headers and tables on the main project page.
    
    ### Added test cases for UI verification:
    
    * **Header tests**: Added `test_headers` to verify that the page
    contains the correct `h1` headers and that the "source_scan" text links
    to the correct URL.
    * **Table count test**: Added `test_for_2_tables` to ensure that the
    page contains exactly two tables.
    * **Summary table tests**: Added `test_summary_table` to validate the
    structure and content of the summary table, including the number of
    rows, headers, and their text.
    * **Repositories table tests**: Added `test_repositories_table` to
    validate the structure and content of the repositories table, including
    the number of rows, headers, and their text.
    
    Fixes #240

--- tests/ui/test_website.py
+++ tests/ui/test_website.py
@@ -13,3 +13,53 @@ def test_title(page: Page) -> None:
     page.goto(PROJECT_URL)
     # Assert
     assert page.title() == "Tech Report | source_scan"
+
+
+def test_headers(page: Page) -> None:
+    """Test the repositories title."""
+    # Act
+    page.goto(PROJECT_URL)
+    # Assert
+    assert page.locator("h1").all_inner_texts() == [
+        "source_scan",
+        "Tech Report",
+    ]
+    assert page.locator("text='source_scan'").get_attribute("href") == f"{PROJECT_URL}/"
+
+
+def test_for_2_tables(page: Page) -> None:
+    """Test the repositories title."""
+    # Act
+    page.goto(PROJECT_URL)
+    # Assert
+    assert page.locator("table").count() == 2
+
+
+def test_summary_table(page: Page) -> None:
+    """Test the summary table."""
+    # Act
+    page.goto(PROJECT_URL)
+    # Assert
+    summary_table = page.locator("table").nth(0)
+    assert summary_table.count() > 0
+    assert summary_table.locator("th").count() == 2
+    assert summary_table.locator("th").all_inner_texts() == [
+        "Technology Badge",
+        "Count",
+    ]
+    assert summary_table.locator("tr").count() == 15
+
+
+def test_repositories_table(page: Page) -> None:
+    """Test the repositories table."""
+    # Act
+    page.goto(PROJECT_URL)
+    # Assert
+    repositories_table = page.locator("table").nth(1)
+    assert repositories_table.count() > 0
+    assert repositories_table.locator("th").count() == 2
+    assert repositories_table.locator("th").all_inner_texts() == [
+        "Project Name",
+        "Technologies And Frameworks",
+    ]
+    assert repositories_table.locator("tr").count() == 31
