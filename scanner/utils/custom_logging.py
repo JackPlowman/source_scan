1802e077ca9da5c36b9ed0eb26c04668d8f04440
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-30 21:19:42 +0100

    update

--- scanner/utils/custom_logging.py
+++ scanner/utils/custom_logging.py
@@ -1,12 +1,32 @@
 from logging import DEBUG, INFO
 from os import getenv
 
-from structlog import configure, make_filtering_bound_logger
+from structlog import (
+    configure,
+    get_logger,
+    make_filtering_bound_logger,
+    stdlib,
+)
+from structlog import (
+    dev as structlog_dev,
+)
+
+logger: stdlib.BoundLogger = get_logger()
 
 
 def set_up_custom_logging() -> None:
     """Setup custom logging for the application."""
     level = INFO
-    if getenv("DEBUG", "false").lower() == "true":
+    if getenv("INPUT_DEBUG", "false").lower() == "true":
         level = DEBUG
-    configure(wrapper_class=make_filtering_bound_logger(level))
+    if getenv("CI"):
+        configure(
+            processors=[
+                structlog_dev.ConsoleRenderer(
+                    exception_formatter=structlog_dev.plain_traceback
+                ),
+            ],
+            wrapper_class=make_filtering_bound_logger(level),
+        )
+    else:
+        configure(wrapper_class=make_filtering_bound_logger(level))
